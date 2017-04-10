2eb532aae7cc736f4cb6cc9d95b332419cebee4a
Sami Mäkinen <sami.makinen@student.tut.fi>
2017-04-10 14:46:39 +0300

    views.py

--- gameservice/views.py
+++ gameservice/views.py
@@ -16,4 +16,115 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-# Create your views here.
+def mainPage(request):
+
+	return render(request, 'main_page.html')
+
+def registerUser(request):
+
+	registered = False
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+		if user_form.is_valid() and profile_form.is_valid():
+			# Save the user's form data to the database.
+			user = user_form.save()
+			# Now we hash the password with the set_password method.
+			# Once hashed, we can update the user object.
+			user.set_password(user.password)
+			#KOMMENTOI POIS SEURAAVAT KAKSI RIVIÄ, JOS ET HALUA ERIKSEEN AKTIVOIDA ACCOUNTTIA!
+			# user.is_active = False
+			# sendConfirmEmail(user)
+			user.save()
+			# Now sort out the UserProfile instance.
+			# Since we need to set the user attribute ourselves, we set commit=False.
+			# This delays saving the model until we're ready to avoid integrity problems.
+			profile = profile_form.save(commit=False)
+			profile.user = user
+			# Did the user provide a profile picture?
+			# If so, we need to get it from the input form and put it in the UserProfile model.
+			#if 'picture' in request.FILES:
+			#	profile.picture = request.FILES['picture']
+			# Now we save the UserProfile model instance.
+			profile.save()
+			# Update our variable to tell the template registration was successful.
+			registered = True
+		# Invalid form or forms - mistakes or something else?
+		# Print problems to the terminal.
+		# They'll also be shown to the user.
+		else:
+			print(user_form.errors, profile_form.errors)
+	# Not a HTTP POST, so we render our form using two ModelForm instances.
+	# These forms will be blank, ready for user input.
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+	# Render the template depending on the context.
+	if not registered:
+		return render(request,
+			'register.html',
+			{
+				'user_form': user_form,
+				'profile_form': profile_form,
+			}
+		)
+
+	else:
+		return render(request,
+			'main_page.html',
+			{
+				'title': "Thank you for registering!",
+				'message': "ebin",
+			}
+		)
+
+def loginUser(request):
+
+	# Like before, obtain the context for the user's request.
+	context = RequestContext(request)
+	# If the request is a HTTP POST, try to pull out the relevant information.
+	if request.method == 'POST':
+		# Gather the username and password provided by the user.
+		# This information is obtained from the login form.
+		username = request.POST['username']
+		password = request.POST['password']
+		# Use Django's machinery to attempt to see if the username/password
+		# combination is valid - a User object is returned if it is.
+		user = authenticate(username=username, password=password)
+		# If we have a User object, the details are correct.
+		# If None (Python's way of representing the absence of a value), no user
+		# with matching credentials was found.
+		if user:
+            return HttpResponseRedirect('/main_page_logged/')
+			# Is the account active? It could have been disabled.
+			# if user.is_active:
+			# 	# If the account is valid and active, we can log the user in.
+			# 	# We'll send the user back to the homepage.
+			# 	login(request, user)
+
+			# else:
+			# 	# An inactive account was used - no logging in!
+			# 	return render(request, 'message.html',
+			# 		{
+			# 			'title': "Account not activated",
+			# 			'message': "Please check your email and activate your account before logging in."
+			# 		})
+		else:
+			# Bad login details were provided. So we can't log the user in.
+			print ("Invalid login details: {0}, {1}".format(username, password))
+			return render(request, 'message.html',
+				{
+					'title': "Invalid login details supplied",
+					'message': "Please check your login information and try again."
+				})
+	# The request is not a HTTP POST, so display the login form.
+	# This scenario would most likely be a HTTP GET.
+	else:
+		# No context variables to pass to the template system, hence the
+			# blank dictionary object...
+			context['request'] = request
+			context['user'] = request.user
+			# If the user is authenticated, we'll sent the user to homepage
+			if request.user.is_authenticated():
+				return HttpResponseRedirect('/main_page_logged/')
+			return render_to_response('login.html', {}, context)
