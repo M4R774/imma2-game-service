fa1132b04734748e01f0d328eec464bb6310e887
Sami Mäkinen <sami.makinen@student.tut.fi>
2017-04-10 15:29:17 +0300

    login and registration

--- gameservice/models.py
+++ gameservice/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length = 255)
+    description = models.TextField(max_length = 255)
+    url = models.URLField(max_lengrth = 255, unique=True)
+    price = models.DecimalField(decimal_places=2)
+    #highscores = models.ForeignKey(Highscore, on_delete=models.CASCADE, related_name="highscores")
+    sales = models.PositiveIntegerField()
+    developer = models.ForeignKey(Developer, related_name="games", blank=True)
+    players = models.ManyToManyField(Player, related_name="games_owned", blank=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+
+class Player(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+
+    def __str__(self):
+        return str(self.user)
+
+class Developer(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+
+
+class Highscore(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField()
--- gameservice/views.py
+++ gameservice/views.py
@@ -25,7 +25,7 @@ def registerUser(request):
 	registered = False
 	if request.method == 'POST':
 		user_form = UserForm(data=request.POST)
-		profile_form = UserProfileForm(data=request.POST)
+		# profile_form = UserProfileForm(data=request.POST)
 		if user_form.is_valid() and profile_form.is_valid():
 			# Save the user's form data to the database.
 			user = user_form.save()
@@ -39,7 +39,7 @@ def registerUser(request):
 			# Now sort out the UserProfile instance.
 			# Since we need to set the user attribute ourselves, we set commit=False.
 			# This delays saving the model until we're ready to avoid integrity problems.
-			profile = profile_form.save(commit=False)
+			# profile = profile_form.save(commit=False)
 			profile.user = user
 			# Did the user provide a profile picture?
 			# If so, we need to get it from the input form and put it in the UserProfile model.
@@ -58,14 +58,14 @@ def registerUser(request):
 	# These forms will be blank, ready for user input.
 	else:
 		user_form = UserForm()
-		profile_form = UserProfileForm()
+		# profile_form = UserProfileForm()
 	# Render the template depending on the context.
 	if not registered:
 		return render(request,
 			'register.html',
 			{
 				'user_form': user_form,
-				'profile_form': profile_form,
+				# 'profile_form': profile_form,
 			}
 		)
 
