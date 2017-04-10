ebd956a1c890a5f583d2b9e49848354062242f3c
Sami Mäkinen <sami.makinen@student.tut.fi>
2017-04-10 15:13:24 +0300

    asd

--- gameservice/views.py
+++ gameservice/views.py
@@ -2,19 +2,20 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-def signup(request):
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=raw_password)
-			login(request, user)
-			return redirect('home')
-		else:
-			form = UserCreationForm()
-			return render(request, 'register.html', {'form': form})
+# def signup(request):
+# 	if request.method == 'POST':
+# 		form = UserCreationForm(request.POST)
+# 		if form.is_valid():
+# 			form.save()
+# 			username = form.cleaned_data.get('username')
+# 			raw_password = form.cleaned_data.get('password1')
+# 			user = authenticate(username=username, password=raw_password)
+# 			login(request, user)
+# 			return redirect('home')
+# 		else:
+# 			form = UserCreationForm()
+#
+# 	return render(request, 'register.html', {'form': form})
 
 def mainPage(request):
 	return render(request, 'main_page.html')
@@ -78,7 +79,6 @@ def registerUser(request):
 		)
 
 def loginUser(request):
-
 
 	context = RequestContext(request)
 
